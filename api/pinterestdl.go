838368fa3f7b630267e9fefd48487a78d4e27c38
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 19:55:38 +0530

    Update pinterestdl.go

--- api/pinterestdl.go
+++ api/pinterestdl.go
@@ -7,7 +7,7 @@ import (
 )
 
 func PinterestDownload(link string) (string, error) {
-  url := "https://horridapi.onrender.com/download_pin?link=" + link
+  url := "https://horridapi.onrender.com/download_pin?url=" + link
   resp, err := http.Get(url)
   if err != nil {
    return "", err
