37aabda715d04a86682dddf434e2d41538c59697
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 14:53:09 +0530

    Create img.go

---
+++ api/img.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strings"
+)
+
+type PinterestResponse struct {
+    Data []struct {
+        URL string `json:"url"` 
+    } `json:"data"` 
+}
+
+func searchPinterest(query string) (PinterestResponse, error) {
+    url := fmt.Sprintf("https://horridapi.onrender.com/pinterest?query=%s", query)
+    resp, err := http.Get(url)
+    if err != nil {
+        return PinterestResponse{}, err
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return PinterestResponse{}, err
+    }
+
+    var result PinterestResponse
+    err = json.Unmarshal(body, &result)
+    if err != nil {
+        return PinterestResponse{}, err
+    }
+
+    return result, nil
+}
