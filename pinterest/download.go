80086a9658326921920c764310fb4502f0848aa1
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 20:08:01 +0530

    Update download.go

--- pinterest/download.go
+++ pinterest/download.go
@@ -29,11 +29,11 @@ func DownloadSend(b *gotgbot.Bot, ctx *ext.Context) error {
 	link := ExtractURL(chk)
 	url := api.PinterestDownload(link)
 	fmt.Println(url)
-        if err != nil {	
-	       message.Reply(b, "opps! An Error Occured Report on @XBOTSUPPORTS", &gotgbot.SendMessageOpts{})
-               fmt.Println(err)
-	       return err
-        }
+     //   if err != nil {	
+	  //     message.Reply(b, "opps! An Error Occured Report on @XBOTSUPPORTS", &gotgbot.SendMessageOpts{})
+          //     fmt.Println(err)
+	//       return err
+   //     }
 	photo := gotgbot.InputMediaPhoto{			
 		Media: gotgbot.InputFileByURL(url),
 	}
