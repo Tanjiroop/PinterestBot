32ea69bd533b7630b6f5a4f4c6afecc1b2f53964
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 12:16:04 +0530

    Update search.go

--- pinterest/search.go
+++ pinterest/search.go
@@ -1,66 +1,65 @@
 package pinterest
 
 import (
-"net/http"
-"encoding/json"   
-"fmt"
-"io/ioutil"		
-"strings"	
-"github.com/PaulSonOfLars/gotgbot/v2"
-"github.com/PaulSonOfLars/gotgbot/v2/ext"	
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strings"
+
+    "github.com/PaulSonOfLars/gotgbot/v2"
+    "github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-
 type PinterestResponse struct {
- Data []struct {
-  URL string json:"url"
- } json:"data"
+    Data []struct {
+        URL string `json:"url"` 
+    } `json:"data"` 
 }
 
 func searchPinterest(query string) (PinterestResponse, error) {
- url := fmt.Sprintf("https://horridapi.onrender.com/pinterest?query=%s", query)
- resp, err := http.Get(url)
- if err != nil {
-  return PinterestResponse{}, err
- }
- defer resp.Body.Close()
+    url := fmt.Sprintf("https://horridapi.onrender.com/pinterest?query=%s", query)
+    resp, err := http.Get(url)
+    if err != nil {
+        return PinterestResponse{}, err
+    }
+    defer resp.Body.Close()
 
- body, err := ioutil.ReadAll(resp.Body)
- if err != nil {
-  return PinterestResponse{}, err
- }
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return PinterestResponse{}, err
+    }
 
- var result PinterestResponse
- err = json.Unmarshal(body, &result)
- if err != nil {
-  return PinterestResponse{}, err
- }
+    var result PinterestResponse
+    err = json.Unmarshal(body, &result)
+    if err != nil {
+        return PinterestResponse{}, err
+    }
 
- return result, nil
+    return result, nil
 }
-
 
 func FindImage(b *gotgbot.Bot, ctx *ext.Context) error {
     message := ctx.Message
     query := strings.Replace(message.Text, "/h", "", -1)
     urls, err := searchPinterest(query)
     if err != nil {
-      fmt.Println(err)
-      message.Reply(b, "Image not found", &gotgbot.SendMessageOpts{})
-      return err
+        fmt.Println(err)
+        message.Reply(b, "Image not found", &gotgbot.SendMessageOpts{})
+        return err
     }
-    
+
     media := make([]gotgbot.InputMedia, 0)
-    for _, url := range urls.Data {
+    for _, item := range urls.Data { 
         media = append(media, gotgbot.InputFileOrString{
-            Media: url.URL,
+            Media: item.URL, 
         })
     }
-    
-    b.SendMediaGroup(
+
+    _, err = b.SendMediaGroup(
         ctx.EffectiveUser.Id,
         media,
         &gotgbot.SendMediaGroupOpts{},
     )
-    return nil
+    return err 
 }
