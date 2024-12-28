af4182043d5e4662238b93455dd793399897b485
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 17:43:34 +0530

    Some features added and bugs and error fixed :(

--- pinterest/download.go
+++ pinterest/download.go
@@ -1,12 +1,67 @@
 package pinterest
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
-
+	"net/http"
+	
+	"golang.org/x/net/html"
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
+
+func ExtractImageUrl(pinterestUrl string) (string, error) {
+	resp, err := http.Get(pinterestUrl)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error fetching content from URL: %s", resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var imageTags []*html.Node
+	var found bool
+
+	finden:
+	for c := doc.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode && c.Data == "img" {
+			for _, a := range c.Attr {
+				if a.Key == "class" && (a.Val == "h-image-fit" || a.Val == "h-unsplash-img") {
+					imageTags = append(imageTags, c)
+					found = true
+					break finden
+				}
+			}
+		}
+	}
+
+	if !found {
+		for c := doc.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.ElementNode && c.Data == "img" {
+				for _, a := range c.Attr {
+					if a.Key == "src" && strings.HasPrefix(a.Val, "https://i.pinimg.com/") {
+						imageTags = append(imageTags, c)
+						break
+					}
+				}
+			}
+		}
+	}
+
+	if len(imageTags) > 0 {
+		return imageTags[0].Attr[0].Val, nil
+	}
+
+	return "", nil
+}
 
 func DownloadSend(b *gotgbot.Bot, ctx *ext.Context) error {
 	message := ctx.EffectiveMessage
@@ -19,8 +74,9 @@ func DownloadSend(b *gotgbot.Bot, ctx *ext.Context) error {
 		return nil
 	}
 
+	link, _ := ExtractImageUrl(chk)
 	photo := gotgbot.InputMediaPhoto{			
-		Media: gotgbot.InputFileByURL(chk),
+		Media: gotgbot.InputFileByURL(link),
 	}
 	_, err := b.SendPhoto(ctx.EffectiveChat.Id, photo.Media, &gotgbot.SendPhotoOpts{})
 	if err != nil {
