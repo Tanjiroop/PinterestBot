4809d248734ccfcb16d3898db8e5bbca3a6988a4
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 14:47:45 +0530

    Update search.go

--- pinterest/search.go
+++ pinterest/search.go
@@ -1,44 +1,11 @@
 package pinterest
 
-import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+import (    
+    "fmt"    
     "strings"
-
     "github.com/PaulSonOfLars/gotgbot/v2"
     "github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
-
-type PinterestResponse struct {
-    Data []struct {
-        URL string `json:"url"` 
-    } `json:"data"` 
-}
-
-func searchPinterest(query string) (PinterestResponse, error) {
-    url := fmt.Sprintf("https://horridapi.onrender.com/pinterest?query=%s", query)
-    resp, err := http.Get(url)
-    if err != nil {
-        return PinterestResponse{}, err
-    }
-    defer resp.Body.Close()
-
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return PinterestResponse{}, err
-    }
-
-    var result PinterestResponse
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        return PinterestResponse{}, err
-    }
-
-    return result, nil
-}
-
 
 func FindImage(b *gotgbot.Bot, ctx *ext.Context) error {
     message := ctx.Message
