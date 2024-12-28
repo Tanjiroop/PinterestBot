aab27ad6e126cd634cf54bf36e7ef5ac85a8b728
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-28 13:59:47 +0530

    Update search.go

--- pinterest/search.go
+++ pinterest/search.go
@@ -44,7 +44,7 @@ func FindImage(b *gotgbot.Bot, ctx *ext.Context) error {
     message := ctx.Message
     split := strings.SplitN(message.GetText(), " ", 2)            
     if len(split) < 2 {     
-        message.Reply(b, "No Query Provied So i can't send Photo, so Please Provide Query Eg: /pinterest Iron man", &gotgbot.SendMessageOpts{})
+        message.Reply(b, "No Query Provied So i can't send Photo, so Please Provide Query Eg: <code>/pinterest Iron man</code>", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML})
         return fmt.Errorf("no query provided")
     }
 
