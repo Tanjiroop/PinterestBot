4808946df0c01c96a2b7c8b6f5c1cfa3eebc2e1d
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-29 17:09:17 +0530

    Update scarp.go

--- api/scarp.go
+++ api/scarp.go
@@ -1,68 +1,67 @@
 package api
 
 import (
-    "fmt"
-    "github.com/PuerkitoBio/goquery"
-    "math/rand"
-    "net/http"
-    "strconv"
-    "strings"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
 )
 
+func FetchWallpapers(query string) []string {
+	imagesData := make([]string, 0)
 
-func FetchWallpapers(query string) {
-    imagesData := make([]string, 0)
+	var url string
+	if query == "" {
+		url = "https://wallpapers.com/search/anime"
+	} else {
+		url = "https://wallpapers.com/search/" + query
+	}
 
-    var url string
-    if query == "" {
-        url = "https://wallpapers.com/search/anime"
-    } else {        
-        url = "https://wallpapers.com/search/" + query
-    }
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching the URL:", err)
+		return imagesData
+	}
+	defer response.Body.Close()
 
-    response, err := http.Get(url)
-    if err != nil {
-        fmt.Println("Error fetching the URL:", err)
-        return imagesData
-    }
-    defer response.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		fmt.Println("Error loading the HTML:", err)
+		return imagesData
+	}
 
-    doc, err := goquery.NewDocumentFromReader(response.Body)
-    if err != nil {
-        fmt.Println("Error loading the HTML:", err)
-        return imagesData
-    }
+	totalPages := 1
+	pageCounter := doc.Find(".page-counter.mobi")
+	if len(pageCounter.Nodes) > 0 {
+		totalPages, _ = strconv.Atoi(strings.Fields(pageCounter.Text())[len(strings.Fields(pageCounter.Text())) - 1])
+	}
 
-    totalPages := 1
-    pageCounter := doc.Find(".page-counter.mobi")
-    if len(pageCounter.Nodes) > 0 {        
-        totalPages, _ = strconv.Atoi(strings.Fields(pageCounter.Text())[len(strings.Fields(pageCounter.Text()))-1])
-    }
+	page := rand.Intn(totalPages) + 1
+	pageURL := url + "?p=" + strconv.Itoa(page)
 
-    page := rand.Intn(totalPages) + 1
-    pageURL := url + "?p=" + strconv.Itoa(page)
+	response, err = http.Get(pageURL)
+	if err != nil {
+		fmt.Println("Error fetching the page URL:", err)
+		return imagesData
+	}
+	defer response.Body.Close()
 
-    response, err = http.Get(pageURL)
-    if err != nil {
-        fmt.Println("Error fetching the page URL:", err)
-        return imagesData
-    }
-    defer response.Body.Close()
+	doc, err = goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		fmt.Println("Error loading the HTML:", err)
+		return imagesData
+	}
 
-    doc, err = goquery.NewDocumentFromReader(response.Body)
-    if err != nil {
-        fmt.Println("Error loading the HTML:", err)
-        return imagesData
-    }
+	doc.Find("li.content-card").Each(func(i int, s *goquery.Selection) {
+		imgTag := s.Find("img")
+		imgURL := imgTag.AttrOr("data-src", "")
+		if imgURL != "" {
+			imageURL := strings.Join(strings.Split(pageURL, "/")[:len(strings.Split(pageURL, "/")) - 1], "/") + imgURL
+			imagesData = append(imagesData, imageURL)
+		}
+	})
 
-    doc.Find("li.content-card").Each(func(i int, s *goquery.Selection) {      
-        imgTag := s.Find("img")        
-        imgURL := imgTag.AttrOr("data-src", "")
-        if imgURL != "" {
-            imageURL := strings.Join(strings.Split(pageURL, "/")[:len(strings.Split(pageURL, "/"))-1], "/") + imgURL
-            imagesData = append(imagesData, imageURL)
-        }
-    })
-
-    return imagesData
+	return imagesData
 }
