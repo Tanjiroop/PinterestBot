ae848d577b329db24f3ea3ce8bc03a08d4f2605c
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-29 11:23:24 +0530

    Update inline.go

--- pinterest/inline.go
+++ pinterest/inline.go
@@ -1,72 +1,72 @@
 package pinterest
 
 import (
- "fmt"
- "strings"
- "math/rand"
+    "fmt"
+    "strings"
+    "math/rand"
 
- "github.com/PaulSonOfLars/gotgbot/v2"
- "github.com/PaulSonOfLars/gotgbot/v2/ext"
- "github.com/Mishel-07/PinterestBot/api"
+    "github.com/PaulSonOfLars/gotgbot/v2"
+    "github.com/PaulSonOfLars/gotgbot/v2/ext"
+    "github.com/Mishel-07/PinterestBot/api"
 )
 
 func FindImageInline(b *gotgbot.Bot, ctx *ext.Context) error {
- query := ctx.InlineQuery.Query
- if query == "" {
-  _, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
-   gotgbot.InlineQueryResultArticle{
-    Id:    fmt.Sprintf("%d", rand.Int()),
-    Title: "No Query Provided",
-    InputMessageContent: &gotgbot.InputTextMessageContent{
-     MessageText: "Please provide a query.",
-    },
-   },
-  }, nil)
-  return err
- }
+    query := ctx.InlineQuery.Query
+    if query == "" {
+        _, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+            gotgbot.InlineQueryResultArticle{
+                Id: fmt.Sprintf("%d", rand.Int()),
+                Title: "No Query Provided",
+                InputMessageContent: &gotgbot.InputTextMessageContent{
+                    MessageText: "Please provide a query.",
+                },
+            },
+        }, nil)
+        return err
+    }
 
- quotequery := strings.Replace(query, " ", "+", -1)
- urls, err := api.SearchPinterest(quotequery)
- if err != nil {
-  fmt.Println(err)
-  _, err = ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
-   gotgbot.InlineQueryResultArticle{
-    Id:    fmt.Sprintf("%d", rand.Int()),
-    Title: "Image not found",
-    InputMessageContent: &gotgbot.InputTextMessageContent{
-     MessageText: "Image not found for your query.",
-    },
-   },
-  }, nil)
-  return err
- }
+    quotequery := strings.Replace(query, " ", "+", -1)
+    urls, err := api.SearchPinterest(quotequery)
+    if err != nil {
+        fmt.Println(err)
+        _, err = ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+            gotgbot.InlineQueryResultArticle{
+                Id: fmt.Sprintf("%d", rand.Int()),
+                Title: "Image not found",
+                InputMessageContent: &gotgbot.InputTextMessageContent{
+                    MessageText: "Image not found for your query.",
+                },
+            },
+        }, nil)
+        return err
+    }
 
- media := make([]gotgbot.InlineQueryResult, 0)
- for _, item := range urls.Data {
-  if item.URL != "" {
-   media = append(media, gotgbot.InlineQueryResultPhoto{
-    Id:    fmt.Sprintf("%d", rand.Int()),
-    PhotoUrl: item.URL,
-    Title:    "Found Image",
-    ThumbnailUrl: item.URL,
-   })
-  } else {
-   fmt.Println("Skipped empty URL")
-  }
- }
+    media := make([]gotgbot.InlineQueryResult, 0)
+    for _, item := range urls.Data {
+        if item.URL != "" {
+            media = append(media, gotgbot.InlineQueryResultPhoto{
+                Id: fmt.Sprintf("%d", rand.Int()),
+                PhotoUrl: item.URL,
+                Title: "Found Image",
+                ThumbnailUrl: item.URL,
+            })
+        } else {
+            fmt.Println("Skipped empty URL")
+        }
+    }
 
- if len(media) == 0 {
-  _, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
-   gotgbot.InlineQueryResultArticle{
-    Title: "No Images Found",
-    InputMessageContent: &gotgbot.InputTextMessageContent{
-     MessageText: "No images found for your query.",
-    },
-   },
-  }, nil)
-  return err
- }
+    if len(media) == 0 {
+        _, err := ctx.InlineQuery.Answer(b, []gotgbot.InlineQueryResult{
+            gotgbot.InlineQueryResultArticle{
+                Title: "No Images Found",
+                InputMessageContent: &gotgbot.InputTextMessageContent{
+                    MessageText: "No images found for your query.",
+                },
+            },
+        }, nil)
+        return err
+    }
 
- _, err = ctx.InlineQuery.Answer(b, media, nil)
- return err
+    _, err = ctx.InlineQuery.Answer(b, media, nil)
+    return err
 }
