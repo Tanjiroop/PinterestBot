eff7d147bb0807ce60695b934bcb866d671acd02
Mishel <131391958+Mishel-07@users.noreply.github.com>
2024-12-29 14:22:11 +0530

    Create bing.go

---
+++ api/bing.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+)
+
+type BingResponse struct {
+    Result []struct {
+        URL string `json:"img"`
+    } `json:"result"` 
+}
+
+func SearchBing(query string) (PinterestResponse, error) {
+    url := fmt.Sprintf("https://horridapi.onrender.com/images?page=7&query=%s", query)
+    resp, err := http.Get(url)
+    if err != nil {
+        return BingResponse{}, err
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return BingResponse{}, err
+    }
+
+    var result BingResponse
+    err = json.Unmarshal(body, &result)
+    if err != nil {
+        return BingResponse{}, err
+    }
+
+    return result, nil
+}
